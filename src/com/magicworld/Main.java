7c5925ad53dadf7d453fdf0bb15164c453843a7c
Jennifer Duval <JDuval@hsk.lan>
2019-09-06 16:02:46 +0200

    Selection des personnages et afectation des caractéristique Terminer.

--- src/com/magicworld/Main.java
+++ src/com/magicworld/Main.java
@@ -1,8 +1,14 @@
 package com.magicworld;
 
 public class Main {
+    static int nbPlayer = 1;
+
     public static void main(String[] args) {
-            Order order = new Order();
-            order.runCharacter();
+        int nbPlayers = 2;
+        while (nbPlayer <= nbPlayers) {
+            SetUp setup = new SetUp();
+            setup.run();
+            nbPlayer++;
+        }
     }
 }
